2731534c3e15b85fbb7eccdeb01af665afb2ed71
chaotzu <darkcore.7@gmail.com>
2023-02-27 13:49:05 -0600

    1.0

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+					
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine(GetPoblacion("Atil"));
+        Console.WriteLine(GetPoblacion("Huépac"));
+	}
+	
+	static String GetPoblacion(String nombrePoblacion)
+    {       
+        var strLines=File.ReadLines("localidades.csv");
+        foreach(var line in strLines)
+        {
+            if(line.Split(',')[1].Equals(nombrePoblacion))
+                return line.Split(',')[2];
+        }
+        return "0";
+    }
+}
